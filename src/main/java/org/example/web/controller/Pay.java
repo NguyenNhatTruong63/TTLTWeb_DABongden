d51402c53bfaf25e85ecbda3962f10f6903641d7
son <21130161@st.hcmuaf.edu.vn>
2024-04-04 21:19:59 +0700

    b

--- src/main/java/org/example/web/controller/Pay.java
+++ src/main/java/org/example/web/controller/Pay.java
@@ -9,7 +9,7 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 
 @WebServlet(name = "Pay", value = "/pay")
-public class Pay extends HttpServlet {
+public class git cPay extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("pay_page.jsp").forward(request, response);
